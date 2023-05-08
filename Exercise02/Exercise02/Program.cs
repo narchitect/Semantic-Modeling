7aad224ccf808418ccd06951c99947bf2554472e
Nayun Kim <ny.kim@tum.de>
2023-05-08 13:49:45 +0200

    initiate the project and the set up
     during the lecture

---
+++ Exercise02/Exercise02/Program.cs
@@ -0,0 +1,19 @@
+﻿using VDS.RDF;
+using VDS.RDF.Parsing;
+
+namespace Exercise02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+
+            //load RDF file
+            NTriplesParser nTriplesParser = new NTriplesParser();
+            nTriplesParser.Load(graph, "TUMgraph.nt");
+
+            Console.ReadKey();
+        }
+    }
+}
